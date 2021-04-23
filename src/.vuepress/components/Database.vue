c2113b753c79a688d70262b9c99259bffdbf04f5
a.kurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-04-23 15:41:23 +0200

    add components database and localization raw

--- src/.vuepress/components/Database.vue
+++ src/.vuepress/components/Database.vue
@@ -1,5 +1,53 @@
 <template>
-    <div>Empty</div>
+    <div>
+      <h2>Database></h2>
+      <p>Most web applications use a database. In this section, we are going to see how Goyave applications can query a database, using the awesome Gorm ORM.</p>
+      <p>Database connections are managed by the framework and are long-lived. When the server shuts down, the database connections are closed automatically. So you don't have to worry about creating, closing or refreshing database connections in your application.</p>
+      <p>Very few code is required to get started with databases. There are some configuration options that you need to change though:</p>
+      <ul>
+        <li>database.connection</li>
+        <li>database.host</li>
+        <li>database.port</li>
+        <li>database.name</li>
+        <li>database.username</li>
+        <li>database.password</li>
+        <li>database.options</li>
+        <li>database.maxOpenConnection</li>
+        <li>database.maxIdleConnection</li>
+        <li>database.maxLifetime</li>
+      </ul>
+      <pre>
+        <code>
+          user := model.User{}
+          db := database.Conn()
+          db.First(&user)
+
+          fmt.Println(user)
+        </code>
+      </pre>
+      <p>Models are usually just normal Golang structs, basic Go types, or pointers of them. sql.Scanner and driver.Valuer interfaces are also supported.</p>
+      <pre>
+        <code>
+          func init() {
+              database.RegisterModel(&User{})
+          }
+
+          type User struct {
+              gorm.Model
+              Name         string
+              Age          sql.NullInt64
+              Birthday     *time.Time
+              Email        string  `gorm:"type:varchar(100);uniqueIndex"`
+              Role         string  `gorm:"size:255"` // set field size to 255
+              MemberNumber *string `gorm:"unique;not null"` // set member number to unique and not null
+              Num          int     `gorm:"autoIncrement"` // set num to auto incrementable
+              Address      string  `gorm:"index:addr"` // create index with name `addr` for address
+              IgnoreMe     int     `gorm:"-"` // ignore this field
+          }
+        </code>
+      </pre>
+      <p>Learn more about using databases in the <a href="https://goyave.dev/guide/basics/database.html">documentation</a></p>
+    </div>
 </template>
 
 <script>
--- src/.vuepress/components/Localization.vue
+++ src/.vuepress/components/Localization.vue
@@ -1,5 +1,71 @@
 <template>
-    <div>Empty</div>
+    <div>
+      <h2>Localization</h2>
+      <p>The Goyave framework provides a convenient way to support multiple languages within your application. Out of the box, Goyave only provides the en-US language.</p>
+      <p>Language files are stored in the resources/lang directory.</p>
+      <pre>
+        <code>
+          .
+          └── resources
+              └── lang
+                  └── en-US (language name)
+                      ├── fields.json (optional)
+                      ├── locale.json (optional)
+                      └── rules.json (optional)
+        </code>
+      </pre>
+      <p>
+        The fields.json file contains the field names translations and their rule-specific messages. Translating field names helps making more expressive messages instead of showing the technical field name to the user. Rule-specific messages let you override a validation rule message for a specific field.
+      </p>
+      <p><b>Example:</b></p>
+      <pre>
+        <code>
+          {
+              "email": {
+                  "name": "email address",
+                  "rules": {
+                      "required": "You must provide an :field."
+                  }
+              }
+          }
+        </code>
+      </pre>
+      <p>The locale.json file contains all language lines that are not related to validation. This is the place where you should write the language lines for your user interface or for the messages returned by your controllers.</p>
+      <p><b>Example:</b></p>
+      <pre>
+        <code>
+          {
+              "product.created": "The product have been created with success.",
+              "product.deleted": "The product have been deleted with success."
+          }
+        </code>
+      </pre>
+      <p>
+        The rules.json file contains the validation rules messages. These messages can have placeholders, which will be automatically replaced by the validator with dynamic values. If you write custom validation rules, their messages shall be written in this file.
+      </p>
+      <p><b>Example:</b></p>
+      <pre>
+        <code>
+          {
+              "integer": "The :field must be an integer.",
+              "starts_with": "The :field must start with one of the following values: :values.",
+              "same": "The :field and the :other must match."
+          }
+        </code>
+      </pre>
+      <p>
+        When an incoming request enters your application, the core language middleware checks if the Accept-Language header is set, and set the goyave.Request's Lang attribute accordingly. Localization is handled automatically by the validator.
+      </p>
+      <p><b>Example:</b></p>
+      <pre>
+        <code>
+          func ControllerHandler(response *goyave.Response, request *goyave.Request) {
+              response.String(http.StatusOK, lang.Get(request.Lang, "my-custom-message"))
+          }
+        </code>
+      </pre>
+      <p>Learn more about localization in the <a href="https://goyave.dev/guide/advanced/localization.html">documentation</a></p>.
+    </div>
 </template>
 
 <script>
